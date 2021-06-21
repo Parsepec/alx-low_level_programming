7569cec11c5d2b18811b2829257529050437efb6
Imran <parse.pecq@gmail.com>
2021-06-21 08:26:38 -0700

    Added 6th low level script

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a text with printf()
+ *
+ * Returns: 0 (Sucess)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
